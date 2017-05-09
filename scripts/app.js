6a3a58f633d904aa867f90e03398069a0f4dcc66
sssnakes <finnan.barry@gmail.com>
2017-05-09 23:27:51 +0200

    side inserts and arrows

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 function loadingBM(){
-  setTimeout('$("#besideMouse").html("Rachel")', 0);
-  setTimeout('$("#besideMouse").html("&amp;")', 500);
-  setTimeout('$("#besideMouse").html("Pierre")', 1000);
+  setTimeout('$("#besideMouse").html("Rachel")' , 0);
+  setTimeout('$("#besideMouse").html("&amp;")'  , 500);
+  setTimeout('$("#besideMouse").html("Pierre")' , 1000);
+  setTimeout('$("#besideMouse").html("and")'    , 1500);
+  setTimeout('$("#besideMouse").html("Rachel")' , 2000);
+  setTimeout('$("#besideMouse").html("et")'     , 2500);
+  setTimeout('$("#besideMouse").html("Pierre")' , 3000);
+  setTimeout('$("#besideMouse").html("en")'     , 3500);
 }
 
 $(document).ready(function(){
@@ -37,25 +42,68 @@ $(document).ready(function(){
   });
 
   function startLoadingBM(){
-    loadingBMint = setInterval(loadingBM, 1500);
+    loadingBMint = setInterval(loadingBM, 4000);
   }
   startLoadingBM();
 
+  // SIDES ----------------------------------------------------------------------
 
-
-  // INTRO SECTION
-  var introH = $('.intro-container').height();
-  var contH  = $(window).height();
-  var travel = -(contH + introH);
-
-  $('.intro-container').css('top', contH);
-  $('.intro-container').animate({
-    top: travel
-  }, 40000);
-
-  $('.intro-text').click(function(){
-    $('.intro-text').fadeOut;
+  $('.st-rachel').mouseenter(function(){
+    var win = -(($(window).width()/2) - 50);
+    $('.sb-rachel').animate({ right: win }, 300);
   });
+
+  // $('.st-rachel').mouseleave(function(){
+  //   var win = ($(window).width()/2) + 60;
+  //   $('.sb-rachel').animate({ right: -win }, 300);
+  // });
+
+  $('.sb-rachel').mouseenter(function(){
+    $('.sb-rachel').animate({ right: 0 }, 300);
+  });
+
+  $('.sb-rachel').mouseleave(function(){
+    var win = ($(window).width()/2) + 60;
+    $('.sb-rachel').animate({ right: -win }, 300);
+  });
+
+
+
+  $('.st-pierre').mouseenter(function(){
+    var win = -(($(window).width()/2) - 50);
+    $('.sb-pierre').animate({ left: win }, 300);
+  });
+
+  // $('.st-rachel').mouseleave(function(){
+  //   var win = ($(window).width()/2) + 60;
+  //   $('.sb-rachel').animate({ right: -win }, 300);
+  // });
+
+  $('.sb-pierre').mouseenter(function(){
+    $('.sb-pierre').animate({ left: 0 }, 300);
+  });
+
+  $('.sb-pierre').mouseleave(function(){
+    var win = ($(window).width()/2) + 60;
+    $('.sb-pierre').animate({ left: -win }, 300);
+  });
+
+
+
+  // INTRO SECTION --------------------------------------------------------------
+
+  // var introH = $('.intro-container').height();
+  // var contH  = $(window).height();
+  // var travel = -(contH + introH);
+  //
+  // $('.intro-container').css('top', contH);
+  // $('.intro-container').animate({
+  //   top: travel
+  // }, 40000);
+  //
+  // $('.intro-text').click(function(){
+  //   $('.intro-text').fadeOut;
+  // });
 
 // END
 });
